f7c41acceb7ae43956f31a8dea93344da79e9916
jesús <jgta1994@yahoo.com>
2019-09-25 18:03:31 -0300

    Proyecto Finalizado
    Se implementó el límite de 8 caracteres y la animación de las teclas.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var Calculadora = {
   componenteB:"",
   operacion:"",
 
+  //DOM
 
   resultado : document.getElementById('display'),
   On : document.getElementById('on'),
@@ -24,9 +25,12 @@ var Calculadora = {
   punto : document.getElementById('punto'),
   igual : document.getElementById('igual'),
   mas : document.getElementById('mas'),
+  botones : document.querySelectorAll('img'),
 
 
   init:function(){
+
+    //funcionalidad
 
     limpio = function(){
       if (Calculadora.resultado.innerHTML === 0 || Calculadora.resultado.innerHTML === '0'){
@@ -35,7 +39,7 @@ var Calculadora = {
     };
 
     function operando(){
-      Calculadora.resultado.innerHTML = 0};
+      Calculadora.resultado.innerHTML = ''};
 
     function restablecer(){
       Calculadora.componenteA = "";
@@ -43,33 +47,34 @@ var Calculadora = {
       Calculadora.operacion = "";
     };
 
-    this.dividido.onclick = function(){
-      Calculadora.componenteA = Calculadora.resultado.innerHTML;
-      Calculadora.operacion =  '/';
-        operando()
 
-    };
+
+    this.dividido.onclick = function(){
+      Calculadora.operacion =  '/';
+      Calculadora.componenteA = Calculadora.resultado.innerHTML;
+        operando();
+      }
 
     this.siete.onclick = function(){
-      if(limpio() == true){
+      if(Calculadora.resultado.innerHTML.length < 8){if(limpio() == true){
         Calculadora.resultado.innerHTML = 7}
         else{
           Calculadora.resultado.innerHTML += 7}
-    };
+    }};
 
     this.ocho.onclick = function(){
-      if(limpio() == true){
+        if(Calculadora.resultado.innerHTML.length < 8){if(limpio() == true){
        Calculadora.resultado.innerHTML = 8}
       else{
         Calculadora.resultado.innerHTML += 8}
-    };
+    }};
 
     this.nueve.onclick = function(){
-      if(limpio() == true){
+        if(Calculadora.resultado.innerHTML.length < 8){if(limpio() == true){
         Calculadora.resultado.innerHTML = 9}
       else{
         Calculadora.resultado.innerHTML += 9}
-    };
+    }};
 
     this.por.onclick = function(){
       Calculadora.componenteA = Calculadora.resultado.innerHTML;
@@ -79,25 +84,25 @@ var Calculadora = {
     };
 
     this.cuatro.onclick = function(){
-      if(limpio() == true){
+        if(Calculadora.resultado.innerHTML.length < 8){if(limpio() == true){
         Calculadora.resultado.innerHTML = 4}
       else{
         Calculadora.resultado.innerHTML += 4}
-    };
+    }};
 
     this.cinco.onclick = function(){
-      if(limpio() == true){
+        if(Calculadora.resultado.innerHTML.length < 8){if(limpio() == true){
         Calculadora.resultado.innerHTML = 5}
       else{
         Calculadora.resultado.innerHTML += 5}
-    };
+    }};
 
     this.seis.onclick = function(){
-      if(limpio() == true){
+        if(Calculadora.resultado.innerHTML.length < 8){if(limpio() == true){
         Calculadora.resultado.innerHTML = 6}
       else{
         Calculadora.resultado.innerHTML += 6}
-    };
+    }};
 
     this.menos.onclick = function(){
       Calculadora.componenteA = Calculadora.resultado.innerHTML;
@@ -106,61 +111,109 @@ var Calculadora = {
     };
 
     this.uno.onclick = function(){
-      if(limpio() == true){
+        if(Calculadora.resultado.innerHTML.length < 8){if(limpio() == true){
         Calculadora.resultado.innerHTML = 1}
       else{
         Calculadora.resultado.innerHTML += 1}
-    };
+    }};
 
     this.dos.onclick = function(){
-      if(limpio() == true){
+        if(Calculadora.resultado.innerHTML.length < 8){if(limpio() == true){Calculadora.componenteA = Calculadora.resultado.innerHTML;
         Calculadora.resultado.innerHTML = 2}
       else{
         Calculadora.resultado.innerHTML += 2}
-    };
+    }};
 
     this.tres.onclick = function(){
-      if(limpio() == true){
+        if(Calculadora.resultado.innerHTML.length < 8){if(limpio() == true){
         Calculadora.resultado.innerHTML = 3}
       else{
         Calculadora.resultado.innerHTML += 3}
-    };
+    }};
 
     this.cero.onclick = function(){
-      if(limpio() == true){
+        if(Calculadora.resultado.innerHTML.length < 8){if(limpio() == true){
         Calculadora.resultado.innerHTML = 0}
       else{
         Calculadora.resultado.innerHTML += 0}
-    };
+    }};
 
+//punto
     this.punto.onclick = function(){
-      if(limpio == true){
-        Calculadora.resultado.innerHTML += '.'
-      }
-      else if(Calculadora.resultado.innerHTML.includes('.')) {
+        if(Calculadora.resultado.innerHTML.length < 8){if(Calculadora.resultado.innerHTML.includes('.')) {
         }
       else{
         Calculadora.resultado.innerHTML += '.'}
-    };
+    }};
 
     this.mas.onclick = function(){
       Calculadora.componenteA = Calculadora.resultado.innerHTML;
       Calculadora.operacion =  '+';
         operando()
      };
+//Igual
+    this.igual.onclick =  function igual(){
+        Calculadora.componenteB = Calculadora.resultado.innerHTML;
+        function resultado(){
+          resDisplay = '';
+          switch (Calculadora.operacion) {
+            case '+':
+              resDisplay = parseFloat(Calculadora.componenteA)  +  parseFloat(Calculadora.componenteB);
+            break;
 
-    this.igual.onclick = function(){
-      Calculadora.componenteB = Calculadora.resultado.innerHTML;
+            case '-':
+              resDisplay = parseFloat(Calculadora.componenteA)  -  parseFloat(Calculadora.componenteB);
+            break;
 
+            case '*':
+              resDisplay = parseFloat(Calculadora.componenteA)  *  parseFloat(Calculadora.componenteB);
+            break;
 
-    };
+            case '/':
+              resDisplay = parseFloat(Calculadora.componenteA)  /  parseFloat(Calculadora.componenteB);
+            break;
+          };
+          restablecer()
+          Calculadora.resultado.innerHTML = resDisplay;
+        };
 
+        resultado()
+      };
+
+//on
     this.On.onclick = function(){
       Calculadora.resultado.innerHTML = 0;
       restablecer()
-  };
+    };
+//signo
+    this.signo.onclick = function(){
+      if (limpio() == true) {
+      }
+      else if(Calculadora.resultado.innerHTML.includes('-')) {
+        Calculadora.resultado.innerHTML = Calculadora.resultado.innerHTML.substring(1)
+        }
+      else {
+        Calculadora.resultado.innerHTML = '-' + Calculadora.resultado.innerHTML
+      }
+    };
+
+
+  //Apariencia
+  for (var i = 0; i < this.botones.length; i++) {
+    this.botones[i].addEventListener('mousedown', function() {
+      this.style.transform="scale(0.95)"
+    })
   }
-}
+  for (var i = 0; i < this.botones.length; i++) {
+    this.botones[i].addEventListener('mouseup', function() {
+      this.style.transform="scale(1)"
+    })
+  }
+
+
+
+  }
+};
 
 
 Calculadora.init()
